4a5d61f3a6b101df1f0d9ca69ddbb35b9e8881ee
Nehemie-Jacques <nehemiesighe1@gmail.com>
2026-02-07 18:22:41 +0100

    Remove GitHub provider and destroy AWS security group resources

--- firewall.tf
+++ firewall.tf
@@ -4,11 +4,11 @@ resource "aws_security_group" "allow_tls" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
-  from_port         = 443
+  security_group_id = "sg-0d047e61c500222ae"
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
   ip_protocol       = "tcp"
-  to_port           = 443
+  to_port           = 100
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {

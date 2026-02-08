f74a4d8133cbb02f9da65e3fc01eedfaaed7a1b2
Nehemie-Jacques <nehemiesighe1@gmail.com>
2026-02-08 21:27:27 +0100

    terraform variable and definition files

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "vpn_ip" {
+  description = "VPN IP address range in CIDR notation"
+}
+
+variable "app_port" {
+  description = "Application port number"
+}
+
+variable "ssh_port" {
+  description = "SSH port number"
+}
+
+variable "ftp_port" {
+  description = "FTP port number"
+}
